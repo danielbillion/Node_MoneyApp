87fdf3e0ae327dbb6990e3717b076b630440b725
Daniel A ogunkoya <danielbillion@yahoo.com>
2019-02-12 10:14:18 +0000

    update config db

--- startup/db.js
+++ startup/db.js
@@ -6,7 +6,7 @@ module.exports = function(){
     const db = config.get('db');
     mongoose.connect(db, { useNewUrlParser: true,useCreateIndex: true, })
 .then(()=>winston.info(`Connected to ${db}...`))
-.catch(err => console.log('could not connect to mongodb'))
+.catch(err => console.log(`could not connect to mongodb ${db}`))
 
     
 }
